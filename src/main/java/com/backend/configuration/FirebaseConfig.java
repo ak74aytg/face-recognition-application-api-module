2b414bebc0dbb43e7a8cace27e3950ca5c498f10
Akshay <95akshay75@gmail.com>
2024-06-30 01:17:27 +0530

    testing firebase push notifications. classpath resolution

--- src/main/java/com/backend/configuration/FirebaseConfig.java
+++ src/main/java/com/backend/configuration/FirebaseConfig.java
@@ -5,16 +5,20 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/fcm.json");
+        Resource resource = new ClassPathResource("fcm.json");
+        InputStream serviceAccount = resource.getInputStream();
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
--- src/main/java/com/backend/service/FirebaseService.java
+++ src/main/java/com/backend/service/FirebaseService.java
@@ -1,22 +1,25 @@
 package com.backend.service;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
 
 @Service
 public class FirebaseService {
 
     private static final String SCOPES = "https://www.googleapis.com/auth/cloud-platform";
-    private static final String SERVICE_ACCOUNT_FILE = "src/main/resources/fcm.json";
+    Resource resource = new ClassPathResource("fcm.json");
 
     public String getAccessToken() throws IOException {
         GoogleCredentials googleCredentials;
-        try (FileInputStream serviceAccountStream = new FileInputStream(SERVICE_ACCOUNT_FILE)) {
-            googleCredentials = GoogleCredentials.fromStream(serviceAccountStream)
+        try (InputStream serviceAccount = resource.getInputStream()) {
+            googleCredentials = GoogleCredentials.fromStream(serviceAccount)
                     .createScoped(Collections.singletonList(SCOPES));
             googleCredentials.refreshIfExpired();
         }
